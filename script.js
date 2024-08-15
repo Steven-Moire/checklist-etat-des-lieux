a7579170959c12c66f7f0cf9951b80886dc6330d
Steven-Moire <150258712+Steven-Moire@users.noreply.github.com>
2024-08-15 12:49:33 +0200

    last try

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Charger les items depuis le fichier JSON
-    fetch('items.json')
-        .then(response => response.json())
-        .then(data => {
-            const checklist = document.getElementById('checklist');
-            data.forEach((item, index) => {
-                const id = `item${index + 1}`;
-                const li = document.createElement('li');
-                li.innerHTML = `<input type="checkbox" id="${id}" data-index="${index}"> <label for="${id}">${item}</label>`;
-                checklist.appendChild(li);
-
-                const checkbox = document.getElementById(id);
-
-                // Vérifier si l'item est enregistré dans le localStorage et restaurer l'état
-                const savedState = localStorage.getItem(id);
-                if (savedState !== null) {
-                    checkbox.checked = savedState === 'true';
-                }
-
-                // Écouter le changement d'état de la case à cocher
-                checkbox.addEventListener('change', function() {
-                    localStorage.setItem(id, checkbox.checked);
-                });
-            });
-        })
-        .catch(error => console.error('Erreur lors du chargement du JSON:', error));
-});
+// Charger les items depuis le fichier JSON
+fetch('items.json')
+    .then(response => response.json())
+    .then(data => {
+        const checklist = document.getElementById('checklist');
+        data.forEach((item, index) => {
+            const li = document.createElement('li');
+            const id = `item${index + 1}`;
+            li.innerHTML = `<input type="checkbox" id="${id}"> <label for="${id}">${item}</label>`;
+            checklist.appendChild(li);
+        });
+    })
+    .catch(error => console.error('Erreur lors du chargement du JSON:', error));
